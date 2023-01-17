46a8930ecf0d6ee10540a7fbd550bc28cabcf47f
LucB310 <lucjwbourdeau@gmail.com>
2023-01-17 13:42:46 -0700

    a

--- algorithm.js
+++ algorithm.js
@@ -2,9 +2,25 @@
 
 function findPrime() {
 
-    println("This will find all the prime numbers within a range of numbers");
+    //println("This will find all the prime numbers within a range of numbers");
 
-    var numOne = prompt("What is the first number? ");
+    var numOne = prompt("This will find all the prime numbers within a range of numbers. What is the first number? ");
     var numTwo = prompt("What is the second number? ");
+
+    var primes = []
+
+    for (var i = numOne; i <= numTwo; i++){
+        var isPrime = true;
+        for (var j = 2; j <= Math.sqrt(i); j++) {
+          if (i % j === 0) {
+            isPrime = false;
+            break;
+          }
+        }
+        if (isPrime) {
+          primes.push(i);
+        }
+      }
+      return primes;
 
 }
