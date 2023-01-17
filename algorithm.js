3b6862b4f65b953526416ff13b9174fac30c93c5
LucB310 <lucjwbourdeau@gmail.com>
2023-01-17 13:23:55 -0700

    adding stuff

---
+++ algorithm.js
@@ -0,0 +1,10 @@
+//Finds all the prime numbers within a range of numbers
+
+function findPrime() {
+
+    println("This will find all the prime numbers within a range of numbers");
+
+    var numOne = prompt("What is the first number? ");
+    var numTwo = prompt("What is the second number? ");
+
+}
